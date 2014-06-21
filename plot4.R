c079c26706851ebed9c294a7de4f6cf3d9abbf44
Mark Drummond <8med5@queensu.ca>
2014-06-21 16:23:55 -0400

    Change to barplot.

--- plot4.R
+++ plot4.R
@@ -6,8 +6,11 @@ SCC <- readRDS("data/Source_Classification_Code.rds")
 SCC <- SCC[grep("coal", SCC$EI.Sector, ignore.case=TRUE),c(1)]
 NEI <- NEI[NEI$SCC %in% SCC,c(4,6)]
 NEI <- aggregate(. ~ year, data=NEI, FUN=sum)
+NEI$Emissions = NEI$Emissions / 1000
 png(filename = "plot4.png", width = 480, height = 480)
-g <- ggplot(data=NEI, aes(x=year, y=Emissions)) +
-  geom_line() + geom_point() + ggtitle("Coal Emissions")
+g <- ggplot(data=NEI, aes(x=factor(year), y=Emissions))
+g <- g + geom_bar(stat = "identity")
+g <- g + labs(title = expression(PM[2.5] * " From Coal Combustion Over Time"))
+g <- g + labs(x = "Year", y = expression(PM[2.5] * " (thousands)"))
 print(g)
 dev.off()
