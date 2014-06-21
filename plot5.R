d778c0e8129dafb24f4589fbd1f906466611ab0c
Mark Drummond <8med5@queensu.ca>
2014-06-21 16:33:57 -0400

    Changed to bar plot.

--- plot5.R
+++ plot5.R
@@ -6,7 +6,9 @@ NEI <- NEI[NEI$fips=="24510",c(4:6)]
 NEI <- NEI[NEI$type=="ON-ROAD",c(1,3)]
 NEI <- aggregate(. ~ year, data=NEI, FUN=sum)
 png(filename = "plot5.png", width = 480, height = 480)
-g <- ggplot(data=NEI, aes(x=year, y=Emissions)) +
-  geom_line() + geom_point() + ggtitle("Motor Vehicle Emissions (Baltimore City)")
+g <- ggplot(data=NEI, aes(x=factor(year), y=Emissions))
+g <- g + geom_bar(stat = "identity")
+g <- g + labs(title = expression(PM[2.5] * " From Motor Vehicle Sources Over Time"))
+g <- g + labs(x = "Year", y = expression(PM[2.5]))
 print(g)
 dev.off()
